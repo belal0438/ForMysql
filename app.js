5429ea0000d064a395d1f9187aee343b888695f7
Belal <mohammadvns121@gmail.com>
2023-05-19 11:50:31 +0530

    Design the User and Product relationship

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,17 +20,43 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res, next) =>{
+    User.findByPk(2)
+    .then(user =>{
+        req.user = user;
+        next();
+    })
+    .catch(err =>console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result =>{
-    // console.log(result);
-    app.listen(3000);
-})
-.catch(err =>{
-    console.log(err)
-})
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+    // .sync({force: true})
+    .sync()
+    .then(result => {
+        return User.findByPk(2);
+        // console.log(result);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Belal', email: 'belal@gmail.com' });
+        }
+        return user
+    })
+    .then(user => {
+        // console.log(user);
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
 
 
