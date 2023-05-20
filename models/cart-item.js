7725e13c94fb7c50330972b19a4a9a4d05ad6bca
Belal <mohammadvns121@gmail.com>
2023-05-21 00:51:46 +0530

    Cart with many to many relationship

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize');
+
 const sequelize = require('../util/database');
 
-const CartItem = sequelize.define('cartItem',{
-
+const CartItem = sequelize.define('cartItem', {
   id: {
     type: Sequelize.INTEGER,
-    autoIncremnet: true,
+    autoIncrement: true,
     allowNull: false,
     primaryKey: true
   },
   quantity: Sequelize.INTEGER
 });
+
 module.exports = CartItem;
--- util/database.js
+++ util/database.js
@@ -1,5 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize ('node-schema', 'root', 'B#5266@belal', {dialect: 'mysql', host: 'localhost'})
+const sequelize = new Sequelize ('node-schema', 'root', 'B#5266@belal',
+{
+  dialect: 'mysql',
+   host: 'localhost'
+  })
 
 module.exports = sequelize;
